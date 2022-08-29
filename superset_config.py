f278171d157511db1f01b7231090976c1a666717
qzmanalytics <jgilroy@qzmarketing.com>
2022-08-29 14:43:24 +0000

    more changes

--- superset_config.py
+++ superset_config.py
@@ -1,5 +1,5 @@
 import os
 
-from flask_appbuilder.security.manager import AUTH_OAUTH, AUTH_DB, AUTH_OID
+from flask_appbuilder.security.manager import AUTH_OAUTH
 SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY")
 SQLALCHEMY_DATABASE_URI=os.getenv("SUPERSET_CONNECTION_STRING")
