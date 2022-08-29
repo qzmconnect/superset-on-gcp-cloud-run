0441ed57e4c184a190ccae47f4fac15db8811acb
qzmanalytics <jgilroy@qzmarketing.com>
2022-08-29 18:45:27 +0000

    modified connection key envvars

--- src/superset_config.py
+++ src/superset_config.py
@@ -6,8 +6,8 @@ from flask_appbuilder.security.manager import AUTH_OAUTH
 
 ENABLE_PROXY_FIX=True
 ROW_LIMIT=5000
-SECRET_KEY=os.getenv("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI=os.getenv("CONNECTION_STRING")
+SECRET_KEY=os.getenv("SUPERSET_SECRET_KEY")
+SQLALCHEMY_DATABASE_URI=os.getenv("SUPERSET_CONNECTION_STRING")
 WTF_CSRF_ENABLED=True
 CSRF_ENABLED=True
 
@@ -31,7 +31,7 @@ OAUTH_PROVIDERS = [{
         "api_base_url": "https://www.googleapis.com/oauth2/v2/",
         "client_kwargs": {"scope": "email profile"},
         "request_token_url": None,
-        "access_token_url": "https://accounts.google.com/o/oauth2/token",
+        "access_token_url": "https://oauth2.googleapis.com/token",
         "authorize_url": "https://accounts.google.com/o/oauth2/auth",
         "jwks_uri": "https://www.googleapis.com/oauth2/v3/certs",
         "authorize_params": {"hd": "qzmarketing.com"}
@@ -65,4 +65,4 @@ OAUTH_PROVIDERS = [{
 #AUTH_USER_REGISTRATION = True
 #AUTH_USER_REGISTRATION_ROLE = "Admin"
 
-#https://oauth2.googleapis.com/token - new url
+#https://accounts.google.com/o/oauth2/token - old access token url
